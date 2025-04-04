fedb7c0bce579b54ae3a743588881956a723f31a
Clayton Gilbert <clayton.gilbert@servicesaustralia.gov.au>
2025-04-04 12:39:07 +1100

    Update CredentialPreviewInfo.swift

--- Sources/dc/CredentialPreviewInfo.swift
+++ Sources/dc/CredentialPreviewInfo.swift
@@ -25,22 +25,22 @@ extension CredentialPreviewInfo {
         var documentTypes: [String] = []
         
         // Create a dictionary from the JSON to check for document types.
-        if let jsonRepresentation = info.jsonRepresentation, let data = try? JSONDecoder().decode([String: AnyCodable].self, from: jsonRepresentation) {
+        if let jsonRepresentation = info.jsonRepresentation, let data = try? JSONDecoder().decode([String: AnyCodable].self, from: jsonRepresentation), let formats = info.formats {
             // Use the formats to determine how to generate the documentTypes.
-            if info.formats.allSatisfy(["mso_mdoc", "mso_mdoc_detail", "mso_mdoc_preview"].contains) {
+            if formats.allSatisfy(["mso_mdoc", "mso_mdoc_detail", "mso_mdoc_preview"].contains) {
                 // For mdoc, get the root level element from the attribute as the documentType.
                 // For Indy get the schema_id.
                 if let item = data["docType"], let type = item.value as? String {
                     documentTypes = [type]
                 }
             }
-            else if info.formats.allSatisfy(["aries/ld-proof-vc-detail@v1.0"].contains) {
+            else if formats.allSatisfy(["aries/ld-proof-vc-detail@v1.0"].contains) {
                 // For JSON-LD, get the array of credential types
                 if let item = data["credential"], let credential = item.value as? [String: Any], let type = credential["type"] as? [String] {
                     documentTypes = type
                 }
             }
-            else if info.formats.allSatisfy(["hlindy-zkp-v1.0"].contains) {
+            else if formats.allSatisfy(["hlindy-zkp-v1.0"].contains) {
                 // For Indy, get the "cred_def_id" from the base64 and the "credential_preview"
                 if let item = data["cred_def_id"], let type = item.value as? String {
                     documentTypes = [type]
